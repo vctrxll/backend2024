2d1089ff62651b4018fcac56d8f85ff3e86b4f32
Vctrxll <viictor.axel11@gmail.com>
2024-12-10 00:15:25 -0600

    comentarios

--- pokemon_game/src/seeders/pokemon.js
+++ pokemon_game/src/seeders/pokemon.js
@@ -1,43 +1,57 @@
+// Importar la conexión a la base de datos desde el archivo correspondiente
 const pool = require("../db/connection");
 
-const pokemonSeeder = async() => {
+/**
+ * Seeder para cargar datos de Pokémon en la base de datos.
+ * Obtiene los datos de la API pública de Pokémon y los inserta en la tabla `pokemon`.
+ */
+const pokemonSeeder = async () => {
+    // Realizar una solicitud a la API de Pokémon para obtener los primeros 151 Pokémon
+    const { results } = await fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=151')
+        .then(res => res.json());
 
-    const {results} = await fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=151').then(res => res.json());
-    const pokemons = await Promise.all (
+    // Obtener detalles adicionales de cada Pokémon usando sus URLs individuales
+    const pokemons = await Promise.all(
         results.map(async (poke) => {
-        const pokemon = await fetch(poke.url).then(res => res.json());
+            const pokemon = await fetch(poke.url).then(res => res.json());
 
-        return {
-            name: poke.name,
-            image: pokemon.sprites.other.dream_world.front_default,
-        }
+            // Retornar un objeto con el nombre y la URL de la imagen de cada Pokémon
+            return {
+                name: poke.name, // Nombre del Pokémon
+                image: pokemon.sprites.other.dream_world.front_default, // URL de la imagen del Pokémon
+            };
         })
     );
-    let conn;
-try{
- conn = await pool.getConnection();
-    await conn.query('SET FOREIGN_KEY_CHECKS = 0');
-    await conn.query('TRUNCATE pokemon');
-    await conn.query('SET FOREIGN_KEY_CHECKS = 1');
-    pokemons.forEach(async(pokemon) => {
-    await conn.query('INSERT INTO pokemon(name, image) VALUES (?, ?)',[
-    pokemon.name,
-    pokemon.image
-])
- });
 
-}catch(err){
-    console.log(err);
-}
-finally{
-    if(conn) conn.end();
-}
+    let conn; // Variable para manejar la conexión a la base de datos
 
+    try {
+        conn = await pool.getConnection(); // Obtener una conexión a la base de datos
 
-    //console.log(pokemons);
-    //console.log(results);
-    //const pokemons = await fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=151').then(res => res.json());
-    //console.log(pokemons);
-}
+        // Deshabilitar las claves foráneas temporalmente
+        await conn.query('SET FOREIGN_KEY_CHECKS = 0');
 
+        // Vaciar la tabla `pokemon`
+        await conn.query('TRUNCATE pokemon');
+
+        // Habilitar nuevamente las claves foráneas
+        await conn.query('SET FOREIGN_KEY_CHECKS = 1');
+
+        // Insertar cada Pokémon en la tabla `pokemon`
+        pokemons.forEach(async (pokemon) => {
+            await conn.query('INSERT INTO pokemon(name, image) VALUES (?, ?)', [
+                pokemon.name, // Nombre del Pokémon
+                pokemon.image, // URL de la imagen del Pokémon
+            ]);
+        });
+    } catch (err) {
+        // Manejar errores y mostrarlos en la consola
+        console.log(err);
+    } finally {
+        // Cerrar la conexión a la base de datos si existe
+        if (conn) conn.end();
+    }
+};
+
+// Exportar el seeder para que pueda ser utilizado en otras partes del proyecto
 module.exports = pokemonSeeder;
--- pokemon_game/src/server.js
+++ pokemon_game/src/server.js
@@ -1,35 +1,62 @@
-const express = require('express');
-const usersRoutes = require('./routes/users');
-const pokemonsRoutes = require('./routes/pokemons');
-const pokemonSeeder = require('./seeders/pokemon');
+// Importar las dependencias necesarias
+const express = require('express'); // Framework para crear aplicaciones web y API en Node.js
+const usersRoutes = require('./routes/users'); // Archivo que contiene las rutas relacionadas con usuarios
+const pokemonsRoutes = require('./routes/pokemons'); // Archivo que contiene las rutas relacionadas con pokémon
+const pokemonSeeder = require('./seeders/pokemon'); // Archivo que inicializa datos relacionados con pokémon en la base de datos o un almacenamiento
 
+/**
+ * Clase `Server` que encapsula la configuración y funcionalidad del servidor.
+ */
 class Server {
     constructor() {
+        // Inicializar una instancia de Express
         this.app = express();
+
+        // Definir el puerto en el que se ejecutará el servidor
         this.port = 3000;
 
+        // Configurar middlewares necesarios
         this.middlewares();
+
+        // Configurar las rutas del servidor
         this.routes();
     }
 
+    /**
+     * Método para configurar middlewares en la aplicación.
+     * En este caso, se utiliza para interpretar solicitudes con cuerpo en formato JSON.
+     */
     middlewares() {
-        this.app.use(express.json());
+        this.app.use(express.json()); // Middleware para habilitar el análisis de datos JSON en las solicitudes
     }
 
+    /**
+     * Método para configurar las rutas principales de la aplicación.
+     * Enlaza las rutas de usuarios y pokémon.
+     */
     routes() {
-        this.app.use('/users', usersRoutes)
-        this.app.use('/pokemons', pokemonsRoutes);
+        this.app.use('/users', usersRoutes); // Asocia el prefijo '/users' con las rutas definidas en `usersRoutes`
+        this.app.use('/pokemons', pokemonsRoutes); // Asocia el prefijo '/pokemons' con las rutas definidas en `pokemonsRoutes`
     }
 
-    seeder(){
-        pokemonSeeder();
+    /**
+     * Método para ejecutar un seeder, que inicializa datos en la base de datos o almacenamiento.
+     * En este caso, se utiliza para agregar datos de ejemplo relacionados con pokémon.
+     */
+    seeder() {
+        pokemonSeeder(); // Ejecuta el seeder para cargar los datos iniciales
     }
 
+    /**
+     * Método para iniciar el servidor.
+     * Escucha en el puerto definido y muestra un mensaje en la consola cuando el servidor está en funcionamiento.
+     */
     start() {
         this.app.listen(this.port, () => {
-            console.log(`Server is running on port ${this.port}`);
+            console.log(`Server is running on port ${this.port}`); // Mensaje de confirmación en la consola
         });
     }
 }
 
+// Exportar la clase `Server` para que pueda ser instanciada y utilizada en otros archivos
 module.exports = Server;
